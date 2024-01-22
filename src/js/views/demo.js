e24b7a872dce607627ba53b6b83d1dc8b9fbab22
Adjani <133295232+Adjani09@users.noreply.github.com>
2024-01-22 19:28:28 +0000

    commit

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-
 import { Context } from "../store/appContext";
-
 import "../../styles/demo.css";
+
 
 export const Demo = () => {
 	const { store, actions } = useContext(Context);
@@ -14,12 +13,19 @@ export const Demo = () => {
 				{store.demo.map((item, index) => {
 					return (
 						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
-							</Link>
+						key={index}
+						className="list-group-item d-flex justify-content-between"
+						style={{ background: item.background }}>
+						<div style={{ display: "flex", alignItems: "center", gap: "30%" }}>
+							<div >
+								<img src="https://picsum.photos/100/100?grayscale" style={{ borderRadius: '50%' }} />
+							</div>
+							<div >
+								<Link to={"/single/" + index}>
+									<span>Link to: {item.title}</span>
+								</Link>
+							</div>
+						</div>
 							{// Conditional render example
 							// Check to see if the background is orange, if so, display the message
 							item.background === "orange" ? (
@@ -27,9 +33,14 @@ export const Demo = () => {
 									Check store/flux.js scroll to the actions to see the code
 								</p>
 							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
+						<div style={{ display: "flex", alignItems: "center"}}>
+							<button className="fa-solid fa-pencil" onClick={() => actions.changeColor(index, "orange")} >
+								Editar
 							</button>
+							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")} style={{ marginLeft: "30px"}}>
+								Eliminar
+							</button>
+						</div>
 						</li>
 					);
 				})}
