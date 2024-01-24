c40debefd7b733e7682fe29b0559a7939bcf137d
Adjani <133295232+Adjani09@users.noreply.github.com>
2024-01-24 20:52:20 +0000

    commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,20 +1,8 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			],
-			saludo: "Hola",
-			contacts: []
+			contacts: [],
+			current_contact: null
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -24,31 +12,44 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 			loadSomeData: () => {
 				fetch(`https://playground.4geeks.com/apis/fake/contact/agenda/contactos_adjani`)
+					.then((response) => response.json())
+					.then(data => setStore({ contacts: data }))
+					.then((data) => {
+						console.log(data)
+						id==false ? setStore({contacts:data}) : setStore({current_contact:data})			
+					})
+			},
+			
+			crear: (contact) => {
+				fetch("https://playground.4geeks.com/apis/fake/contact/", {
+					method: "POST",
+					body: JSON.stringify(contact),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
 				.then((response) => response.json())
-				.then(data => setStore({contacts: data}))
+				.then((data) => console.log(data))
 			},
 
 			eliminar: (indexDelete) => {
 				const store = getStore();
 				console.log(store.contacts.filter((item, index) => index != indexDelete))
-				
-				setStore({contacts: store.contacts.filter((item, index) => index != indexDelete)})
-				
+				setStore({ contacts: store.contacts.filter((item, index) => index != indexDelete) })
+
 				var requestOptions = {
 					method: 'DELETE',
 					redirect: 'follow'
-				  };
-				  
+				};
 				fetch("https://playground.4geeks.com/apis/fake/contact/" + indexDelete, requestOptions)
-				.then(response => response.text())
-				.then(result => console.log(result))
-				.then(() => {
-					fetch(`https://playground.4geeks.com/apis/fake/contact/agenda/contactos_adjani`)
-					.then((response) => response.json())
-					.then(data => setStore({contacts: data}))
-				})
+					.then(response => response.text())
+					.then(result => console.log(result))
+					.then(() => {
+						fetch(`https://playground.4geeks.com/apis/fake/contact/agenda/contactos_adjani`)
+							.then((response) => response.json())
+							.then(data => setStore({ contacts: data }))
+					})
 			},
-
 			changeColor: (index, color) => {
 				//get the store
 				const store = getStore();
@@ -61,7 +62,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
-			}
+			},
 		}
 	};
 };
---
+++ src/js/views/Contact.js
@@ -0,0 +1,44 @@
+import React, {useContext} from "react";
+import { Link } from "react-router-dom";
+import "../../styles/Contact.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencil, faTrashCan, faLocationDot, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { Context } from "../store/appContext";
+
+
+export const Contacts = () => {
+	const { store, actions } = useContext(Context);
+	
+		return (
+			<div className="contactsPage container">
+				<Link to="/Add">
+					<button className="addContact btn btn-success">Add new contact</button>
+				</Link>
+						{store.contacts.map((item, index) => {
+							return (
+								<div className="contacts" key={index}>
+									<div className="contact">
+										<div className="image">
+										<img src="https://picsum.photos/100/100?grayscale"/>
+										</div>
+										<div className="information">
+											<div>
+												<h5>{item.full_name}</h5>
+												<p><FontAwesomeIcon icon={faLocationDot} /> {item.address}</p>
+												<p><FontAwesomeIcon icon={faPhone} /> {item.phone}</p>
+												<p><FontAwesomeIcon icon={faEnvelope} /> {item.email}</p>
+											</div>
+										</div>
+										<div className="editContact">
+											<Link to={"/Edit/"+ item.id}>
+											<button><FontAwesomeIcon icon={faPencil} className="pencil"/></button>
+											</Link>
+											<button onClick={() => actions.deleteContact(item.id)}><FontAwesomeIcon icon={faTrashCan} className="trash"/></button>
+										</div>
+									</div>
+								</div>
+									);
+								})}	
+			</div>
+	);
+	};
