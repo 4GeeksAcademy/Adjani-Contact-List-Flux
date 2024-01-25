7280ba65e2377c4c3c4c55fadd646859f0d04dc9
Adjani <133295232+Adjani09@users.noreply.github.com>
2024-01-25 19:44:05 +0000

    commit

--- src/js/views/AddContact.js
+++ src/js/views/AddContact.js
@@ -1,82 +1,87 @@
-import React, {useContext, useState, useEffect} from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import "../../styles/AddContact.css";
 import { Context } from "../store/appContext";
 
 export const AddContact = (props) => {
 
-        const { store, actions } = useContext(Context);
-        const params = useParams();
-        const [fullName, setFullName] = useState("");
-        const [phone, setPhone] = useState("");
-        const [email, setEmail] = useState("");
-        const [address, setAddress] = useState("");
-        
-		const newContact = {
-			full_name: fullName, 
-			email: email, 
-			agenda_slug: "contactos_adjani", 
-			address: address, 
-			phone: phone };
+    const { store, actions } = useContext(Context);
+    const params = useParams();
+    const [fullName, setFullName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
 
-        function saveButton () {
-            e.preventDefault();
-            if (props.opt == "add") {
-                return actions.crear(newContact)
-            }
-            if (props.opt == "edit") {
-                
-                    let requestOptions = {
-                        method: 'PUT',
-                        redirect: 'follow',
-                        body: JSON.stringify(newContact),
-                        headers: {
-                                    "Content-Type": "application/json"
-                                }
-                            };
-                      
-                       fetch("https://playground.4geeks.com/apis/fake/contact/" + params.id, requestOptions)
-                        .then(response => response.json())
-                        .then(result => console.log(result)) 
-            }
+    const newContact = {
+        full_name: fullName,
+        email: email,
+        agenda_slug: "contactos_adjani",
+        address: address,
+        phone: phone
+    };
+
+    function saveButton(e) {
+        e.preventDefault();
+        if (props.opt == "add") {
+            return actions.crear(newContact)
+                .then(() => {
+                    history.push("/");
+                })
         }
-        
-        useEffect (()=> {
-            actions.loadSomeData(params.id)
-        }, [])
+        if (props.opt == "edit") {
 
-        useEffect (()=> {
-            setFullName(store.current_contact?.full_name),
+            let requestOptions = {
+                method: 'PUT',
+                redirect: 'follow',
+                body: JSON.stringify(newContact),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            };
+
+            fetch("https://playground.4geeks.com/apis/fake/contact/" + params.id, requestOptions)
+                .then(response => response.json())
+                .then(result => console.log(result))
+            history.push("/");
+        }
+    }
+
+    useEffect(() => {
+        actions.loadSomeData(params.id);
+    }, [params.id, actions]);
+
+    useEffect(() => {
+        setFullName(store.current_contact?.full_name),
             setPhone(store.current_contact?.phone),
             setEmail(store.current_contact?.email),
             setAddress(store.current_contact?.address)
-        }, [store.current_contact])
-    
-	return (
-		<form action="/">
-			<h1 className="d-flex justify-content-center mt-5">{props.opt == "add" ? "Add a new contact" : "Edit contact"}</h1>
-			<div className="mb-3 mx-5">
+    }, [store.current_contact])
+
+    return (
+        <form action="/">
+            <h1 className="d-flex justify-content-center mt-5">{props.opt == "add" ? "Add a new contact" : "Edit contact"}</h1>
+            <div className="mb-3 mx-5">
                 <label htmlFor="Name" className="form-label">Full Name</label>
-                <input value= {fullName} onChange={(e) => {setFullName(e.target.value)}} name="Name" type="text" className="form-control" placeholder="Full Name"/>
+                <input value={fullName} onChange={(e) => { setFullName(e.target.value) }} name="Name" type="text" className="form-control" placeholder="Full Name" />
             </div>
             <div className="mb-3 mx-5">
                 <label htmlFor="Email" className="form-label">Email</label>
-                <input value= {email} onChange={(e) => {setEmail(e.target.value)}} name="Email" type="email" className="form-control" placeholder="Enter email" />
+                <input value={email} onChange={(e) => { setEmail(e.target.value) }} name="Email" type="email" className="form-control" placeholder="Enter email" />
             </div>
             <div className="mb-3 mx-5">
                 <label htmlFor="Phone" className="form-label">Phone</label>
-                <input value= {phone} onChange={(e) => {setPhone(e.target.value)}} name="Phone" type="tel" className="form-control" placeholder="Enter phone" />
+                <input value={phone} onChange={(e) => { setPhone(e.target.value) }} name="Phone" type="tel" className="form-control" placeholder="Enter phone" />
             </div>
-			<div className="mb-3 mx-5">
+            <div className="mb-3 mx-5">
                 <label htmlFor="Address" className="form-label">Address</label>
-                <input value= {address} onChange={(e) => {setAddress(e.target.value)}} name="Address" type="tel" className="form-control" placeholder="Enter address" />
+                <input value={address} onChange={(e) => { setAddress(e.target.value) }} name="Address" type="tel" className="form-control" placeholder="Enter address" />
             </div>
-			<div className="d-grid gap-2">
+            <div className="d-grid gap-2">
                 <button onClick={(e) => saveButton(e)} className="btn btn-primary mx-5 mt-3">Save</button>
-				<Link to="/" className="ms-5">
-					or get back to contacts
-				</Link>
-			</div>
+                <Link to="/" className="ms-5">
+                    or get back to contacts
+                </Link>
+            </div>
         </form>
-	);
+    );
 };
